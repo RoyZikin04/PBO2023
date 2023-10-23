3cb5953e87b7bad19c96ab9a14da6058ffd5c51f
RoyZikin04 <148208743+RoyZikin04@users.noreply.github.com>
2023-10-23 15:58:52 +0700

    Add files via upload

---
+++ Pertemuan1 /Hasil/Balok.py
@@ -0,0 +1,11 @@
+#menghitung luas dan volume kubus 
+panjang = 6
+lebar = 8
+tinggi = 5
+
+#rumus 
+luas = 2 * (panjang * lebar + panjang * tinggi + lebar * tinggi)
+volume = panjang * lebar * tinggi
+
+print("Luas balok adalah:", luas)
+print("Volume balok adalah:", volume)
